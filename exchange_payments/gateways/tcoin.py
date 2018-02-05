60dd913c05b398c68cdfad4f8f52858142498685
Juliano Gouveia <juliano@neosacode.com>
2018-02-05 17:00:22 -0200

    Add tcoin gateway

---
+++ exchange_payments/gateways/tcoin.py
@@ -0,0 +1,28 @@
+from bitcoin.rpc import Proxy
+from django.conf import settings
+
+
+RPC_USERNAME = settings.TCOIN_RPC_USERNAME
+RPC_PASSWORD = settings.TCOIN_RPC_PASSWORD
+RPC_URL = settings.TCOIN_RPC_URL
+RPC_PROXY = settings.TCOIN_RPC_PROXY
+
+rpc_proxy = Proxy(service_url=RPC_URL, rpc_username=RPC_USERNAME, rpc_password=RPC_PASSWORD, proxy=RPC_PROXY)
+
+
+class Gateway:
+    def create_transaction(self, buyer_email, amount, currency='BTC', currency1='BTC', currency2='BTC'):
+        pass
+
+    def get_transactions(self):
+        pass
+
+    def get_transaction(self, transaction_id):
+        pass
+
+    def get_address(self, account):
+        address = rpc_proxy.getnewaddress()
+        return address['result']
+
+    def can_deposit(self, account, data):
+        pass
