f5dd473c823c3781cc365006559249d45a8a597f
Juliano Gouveia <juliano@neosacode.com>
2018-03-01 13:16:38 -0300

    Change withdraw e-mail template

--- exchange_payments/management/commands/tcoin_confirm_deposits.py
+++ exchange_payments/management/commands/tcoin_confirm_deposits.py
@@ -1,3 +1,4 @@
+import time
 from decimal import Decimal
 
 from django.core.management.base import BaseCommand
@@ -12,39 +13,42 @@ class Command(BaseCommand):
 	help = 'Confirm TCOIN deposits'
 
 	def handle(self, *args, **options):
-		txs = rpc_proxy._call('listtransactions')
+		while True:
+			txs = rpc_proxy._call('listtransactions')
 
-		for tx in txs:
-			with transaction.atomic():
-				# Valida se o tipo e o valor da transacao atendedem os requisitos para deposito
-				if tx['category'] != 'receive' or tx['amount'] <= 0:
-					continue
+			for tx in txs:
+				with transaction.atomic():
+					# Valida se o tipo e o valor da transacao atendedem os requisitos para deposito
+					if tx['category'] != 'receive' or tx['amount'] <= 0:
+						continue
 
-				# Pega a conta TCOIN do usuario usando a carteira dele
-				currency = Currencies.objects.get(symbol=settings.TCOIN_CURRENCY_SYMBOL)
-				accounts = Accounts.objects.filter(currency=currency, deposit_address=tx['address'])
+					# Pega a conta TCOIN do usuario usando a carteira dele
+					currency = Currencies.objects.get(symbol=settings.TCOIN_CURRENCY_SYMBOL)
+					accounts = Accounts.objects.filter(currency=currency, deposit_address=tx['address'])
 
-				# Se a conta para a carteira nao existir, vai para a proxima transacao
-				if not accounts.exists():
-					continue
+					# Se a conta para a carteira nao existir, vai para a proxima transacao
+					if not accounts.exists():
+						continue
 
-				account = accounts.first()
+					account = accounts.first()
 
-				# Valida se a transacao ja foi processada
-				statements = Statement.objects.filter(account=account, tx_id=tx['txid'])
-				if statements.exists():
-					continue
+					# Valida se a transacao ja foi processada
+					statements = Statement.objects.filter(account=account, tx_id=tx['txid'])
+					if statements.exists():
+						continue
 
-				# Deposita o valor para o usuario
-				statement = Statement()
-				statement.account = account
-				statement.amount = Decimal(tx['amount'])
-				statement.description = 'Deposit'
-				statement.type = Statement.TYPES.deposit
-				statement.tx_id = tx['txid']
-				statement.save()
+					# Deposita o valor para o usuario
+					statement = Statement()
+					statement.account = account
+					statement.amount = Decimal(tx['amount'])
+					statement.description = 'Deposit'
+					statement.type = Statement.TYPES.deposit
+					statement.tx_id = tx['txid']
+					statement.save()
 
-				account.deposit += Decimal(tx['amount'])
-				account.save()
+					account.deposit += Decimal(tx['amount'])
+					account.save()
 
-				print('Pagando {} para a conta TCOIN do usuario {}'.format(tx['amount'], account.user.username))
+					print('Pagando {} para a conta TCOIN do usuario {}'.format(tx['amount'], account.user.username))
+
+			time.sleep(1)
